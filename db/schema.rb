dc2d4b6035d3d302905fea3129e02092acf472df
lamckenneth <lam.c.kenneth@berkeley.edu>
2015-04-28 17:43:36 -0700

    sup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427225318) do
+ActiveRecord::Schema.define(version: 20150429003629) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150427225318) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-cake = Food.create(name: 'Carrot Cake', imageurl: 'http://www.villageinn.com/i/pies/profile/carrotcake_main1.jpg', description: "A moist cake studded with shredded carrots, pineapple, walnuts, and raisins covered with cream cheese frosting and finished with chopped almonds.")
+cake = Food.create(name: 'Carrot Cake', imageurl: 'http://www.villageinn.com/i/pies/profile/carrotcake_main1.jpg', description: "A moist cake studded with shredded carrots, pineapple, walnuts, and raisins covered with cream cheese frosting and finished with chopped almonds.", user_id: 1)
 
-cookie = Food.create(name: 'Ice Cream Cookie Sandwich', imageurl: 'http://40.media.tumblr.com/66fa8f0a43dbf4a5a2759aca03049905/tumblr_mo12txUKKG1qfd0s0o1_1280.jpg', description: "Better than CREAM.")
+cookie = Food.create(name: 'Ice Cream Cookie Sandwich', imageurl: 'http://40.media.tumblr.com/66fa8f0a43dbf4a5a2759aca03049905/tumblr_mo12txUKKG1qfd0s0o1_1280.jpg', description: "Better than CREAM.", user_id: 1)
 
-burger = Food.create(name: 'Cheeseburger', imageurl: 'http://41.media.tumblr.com/tumblr_m3qsvi40av1qg5ngpo1_500.jpg', description: "Gives you the best heart attack life has to offer.")
+burger = Food.create(name: 'Cheeseburger', imageurl: 'http://41.media.tumblr.com/tumblr_m3qsvi40av1qg5ngpo1_500.jpg', description: "Gives you the best heart attack life has to offer.", user_id: 1)
 
-comment = Comment.create(content: 'Hi!', food_id: 1)
+comment = Comment.create(content: 'Hi!', food_id: 1, user_id: 1)
 
-comment_again = Comment.create(content: 'I have arrived!', food_id: 1)
+comment_again = Comment.create(content: 'I have arrived!', food_id: 1, user_id: 1)
+
+user = User.create(email: 'yoyo@yoyo.yoyo', encrypted_password: 'yoyoyoyo')
